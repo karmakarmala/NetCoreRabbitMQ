7ae632736e8bb3a3ff8861fb80df5864c4eb637f
Mala Karmakar <mala.karmakar@accessworld.com>
2022-04-07 09:56:08 +0800

    Add project files.

---
+++ Publisher/Controllers/DeviceSensorController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using Publisher.Models;
+
+namespace Publisher.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DeviceSensorController :ControllerBase
+    {
+        [HttpPost("Post")]
+        public void Post([FromBody] DeviceSensor sensor)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "deviceStatusSampleQueue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = String.Format("Device Name : {0} , ModelNo : {1} , DeviceStatus :{2} , MotorTemperature :{3}, BlackCartridgePercentage :{4},CyanCartridgePercentage :{5}, MagentaCartridgePercentage :{6}, YellowCartridgePercentage :{7}", sensor.DeviceName, sensor.ModelNo, sensor.DeviceStatus, sensor.MotorTemperature, sensor.BlackCartridgePercentage, sensor.CyanCartridgePercentage, sensor.MagentaCartridgePercentage, sensor.YellowCartridgePercentage);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "deviceStatusSampleQueue",
+                                     basicProperties: null,
+                                     body: body);
+            }
+        }
+    }
+}
---
+++ Publisher/Startup.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+
+
+namespace Publisher
+{
+
+        public class Startup
+        {
+            public Startup(IConfiguration configuration)
+            {
+                Configuration = configuration;
+            }
+
+            public IConfiguration Configuration { get; }
+
+            // This method gets called by the runtime. Use this method to add services to the container.
+            public void ConfigureServices(IServiceCollection services)
+            {
+                services.AddControllers();
+
+            // Register Swagger
+            services.AddSwaggerGen(options =>
+                {
+                    options.SwaggerDoc("v1", new OpenApiInfo
+                    {
+                        Version = "v1",
+                        Title = "Swagger API",
+                        Description = "An ASP.NET Core Web API for RabbitMQ demo",
+                        TermsOfService = new Uri("https://example.com/terms"),
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Example Contact",
+                            Url = new Uri("https://example.com/contact")
+                        },
+                        License = new OpenApiLicense
+                        {
+                            Name = "Example License",
+                            Url = new Uri("https://example.com/license")
+                        }
+                    });
+                });
+    
+            }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+
+                app.UseSwagger(c =>
+                {
+                    c.SerializeAsV2 = true;
+                });
+
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger CRUD API v1");
+                });
+
+
+                app.UseHttpsRedirection();
+
+                app.UseRouting();
+
+                app.UseAuthorization();
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+            }
+        }
+        }
+    }
+
